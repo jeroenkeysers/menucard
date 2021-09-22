2dda629509bfcb692c1d02bd8c3dc75d79d50c9d
Jeroen Keysers <r0840282@student.thomasmore.be>
2021-09-22 17:13:13 +0200

    6.3

--- src/components/ProductLine.js
+++ src/components/ProductLine.js
@@ -1,8 +1,22 @@
-export function ProductLine(props) {
+export function ProductSize(props) {
     const {product} = props;
     const s = product.size ? ` - ${product.size}cl`:"";
+    return <div>
+        <div className="productSize">{s}</div>
+    </div>
+}
+export function ProductNote(props){
+    const {product} = props;
+    const s = product.note ? `${product.note}` : "";
+    return <div>
+    <div className="note">{s}</div>
+    </div>
+}
+export function ProductLine(props) {
+    const {product} = props;
+
     return <div className="productLine">
-        <div>{product.name} <span className="productSize">{s}</span></div>
+        <div>{product.name}<ProductSize product={product}/><ProductNote product={product}/></div>
         <div>{product.price}&euro;</div>
 
     </div>;
